691e5e4571a572b50243f9e75b563a7920f2e1c3
partho <souravpk.sp@gmail.com>
2018-03-15 12:36:13 +0600

    File renames

--- resources/views/base_layout.blade.php
+++ resources/views/base_layout.blade.php
@@ -38,7 +38,7 @@
                         <li><a href="/match/create">Create Match</a></li>
                         <li><a href="/login">login</a></li>
                         <li><a href="/register">Register</a></li>
-                        <li><a href="/adminpanel"></a>Admin Panel</li>
+                        <li><a href="/adminpanel">Admin Panel</a></li>
                     </ul>
                 </div>
                 <!-- /.navbar-collapse -->
--- resources/views/match/admin/mygames.blade.php
+++ resources/views/match/admin/mygames.blade.php
@@ -4,7 +4,7 @@
 @section('page_content')
 
     {{--CSS--}}
-    <link rel="stylesheet" href="/assets/css/homepage.css">
+    <link rel="stylesheet" href="/assets/css/panel.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
