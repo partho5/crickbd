f39dc00e852fcb122ebc44b68459db3c2d1d19bb
partho <souravpk.sp@gmail.com>
2018-03-15 12:45:02 +0600

    File renames

--- resources/views/match/admin/mygames.blade.php
+++ resources/views/match/admin/mygames.blade.php
@@ -18,12 +18,21 @@
     <div class="col-md-8 col-md-offset-2"style="padding: 0;">
         <section id="main-body" style="margin-top: 50px;">
             <div id="today-match">Running</div>
-            <a href="/match"><div class="match-detail-wrap">
-                    <p class="team-name">EEE <span style="color: #636b6f;">vs</span> CSE</p>
-                    <p class="team-active">EEE <span class="run-active">125</span>/<span class="wicket">6</span> <span class="active-over"> (5.2 over)</span></p>
-                    <p class="inactive-team">Karachi Kings 225/6 (20 over)</p>
-                    <p class="inactive-team">Islamabad United need <span class="run-active">100</span> runs in <span class="ball-left">59</span> balls</p>
-                </div></a>
+            <a href="/match">
+                <div class="col-md-12">
+                    <div class="col-md-8">
+                        <div class="match-detail-wrap">
+                            <p class="team-name">EEE <span style="color: #636b6f;">vs</span> CSE</p>
+                            <p class="team-active">EEE <span class="run-active">125</span>/<span class="wicket">6</span> <span class="active-over"> (5.2 over)</span></p>
+                            <p class="inactive-team">Karachi Kings 225/6 (20 over)</p>
+                            <p class="inactive-team">Islamabad United need <span class="run-active">100</span> runs in <span class="ball-left">59</span> balls</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        sldjhsekjhfkh
+                    </div>
+                </div>
+            </a>
 
             <div id="upcoming-match">Upcoming</div>
 
