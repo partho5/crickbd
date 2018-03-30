33823ead48a26e1eada1f48ac0d5c67d1477be0f
partho <souravpk.sp@gmail.com>
2018-03-31 00:09:16 +0600

    homepage todays match commented

--- resources/views/basic/homepage.blade.php
+++ resources/views/basic/homepage.blade.php
@@ -17,13 +17,13 @@
     {{--<div class="col-md-12">--}}
         <div class="col-md-8 col-md-offset-2"style="padding: 0;">
             <section id="main-body" style="margin-top: 50px;">
-                <div id="today-match">Today's Match</div>
-                <a href="/match"><div class="match-detail-wrap">
-                        <p class="team-name">EEE <span style="color: #636b6f;">vs</span> CSE</p>
-                        <p class="team-active">EEE <span class="run-active">125</span>/<span class="wicket">6</span> <span class="active-over"> (5.2 over)</span></p>
-                        <p class="inactive-team">Karachi Kings 225/6 (20 over)</p>
-                        <p class="inactive-team">Islamabad United need <span class="run-active">100</span> runs in <span class="ball-left">59</span> balls</p>
-                    </div></a>
+                {{--<div id="today-match">Today's Match</div>--}}
+                {{--<a href="/match"><div class="match-detail-wrap">--}}
+                        {{--<p class="team-name">EEE <span style="color: #636b6f;">vs</span> CSE</p>--}}
+                        {{--<p class="team-active">EEE <span class="run-active">125</span>/<span class="wicket">6</span> <span class="active-over"> (5.2 over)</span></p>--}}
+                        {{--<p class="inactive-team">Karachi Kings 225/6 (20 over)</p>--}}
+                        {{--<p class="inactive-team">Islamabad United need <span class="run-active">100</span> runs in <span class="ball-left">59</span> balls</p>--}}
+                    {{--</div></a>--}}
 
                 <div id="upcoming-match">Upcoming Match</div>
                 @if(sizeof($up_matches)>=1)
@@ -46,7 +46,7 @@
 
                 <a href=""><div class="match-detail-wrap">
                         <p class="team-name">EEE <span style="color: #636b6f;">vs</span> CSE</p>
-                        <p class="result">EEE won by 5 wicket or 23 run</p>
+                        <p class="result">EEE won by 5 wicket</p>
                     </div></a>
 
             </section>
