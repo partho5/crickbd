feb3baffcbb36909bbb8459d09e4b4934c5c7c17
partho <souravpk.sp@gmail.com>
2018-04-02 22:47:46 +0600

    scoreboard added

--- resources/views/layouts/decide_winner.blade.php
+++ resources/views/layouts/decide_winner.blade.php
@@ -1,6 +1,14 @@
-<h1>
-    hello
-</h1>
-<h1>
-    We found the match winner.
-</h1>
+<div class="final-result-wrapper">
+    <form class="final-result text-center">
+        <p class="text-center final-result-heading">Final Result</p>
+        <div class="result-details-wrap">
+            <p class="team-name">EEE <span style="color: #636b6f;">vs</span> ACCE</p>
+            <p class="won-by"><span>eee won the toss and choose to bat</span><br>
+            <span>eee won by <span class="wicket">5</span> wicket or <span class="run-active">34</span> runs</span> </p>
+            <p><span class="over">20 </span>overs match</p>
+            <p>Venue: <span class="venue"> DU</span></p>
+            <p>Started at <span class="start-date-time"> 12-12-2018</span></p>
+            <a href="">Full details</a>
+        </div>
+    </form>
+</div>
